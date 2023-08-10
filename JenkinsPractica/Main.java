8411b31a83893e4ddd85904cd09d30417fc39411
jesuZzz147 <izaguirre.jesus.147@gmail.com>
2023-08-10 15:49:18 -0300

    Ahorasi

--- JenkinsPractica/Main.java
+++ JenkinsPractica/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo, desde Java");
         System.out.println("La conexion con GitHub se ha hecho");
-        System.out.println("Me compilo Jenkins");
+        System.out.println("Me compilo Jenkins uwu");
     }
 }
