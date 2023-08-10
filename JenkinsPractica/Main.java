248d14325720028f98d52190079aa9b171b1a728
jesuZzz147 <izaguirre.jesus.147@gmail.com>
2023-08-10 14:09:16 -0300

    Jenkins2

--- JenkinsPractica/Main.java
+++ JenkinsPractica/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo, desde Java");
+        System.out.println("La conexion con GitHub se ha hecho");
     }
 }
